bd64d41cc997b8b8159efa651a5dcc4490a68d68
Felipe Corniani de Genaro <felipegenaro@MacBook-Air.local>
2018-02-26 18:25:22 -0300

    modify the http status and more comments in the code

--- api_rest_full.py
+++ api_rest_full.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-import uuid									# universally unique identifier		# generate the public_id
-from werkzeug.security import generate_password_hash, check_password_hash		# to pass the password for an hask encode and check the hash
-import jwt									# json web token					# generate the token. PyJWT
-import datetime
 from functools import wraps
+from flask import Flask, request, jsonify, make_response 						# http request, like headers and authorization using http protocols and encode the resposes to json
+from flask_sqlalchemy import SQLAlchemy 										# connection to your DB and query uses
+from werkzeug.security import generate_password_hash, check_password_hash		# to pass the password for an hask encode and check the hash
+import uuid									# universally unique identifier		# generate the public_id
+import jwt									# json web token					# generate the token. PyJWT
+import datetime								# get the time and calculate date dif
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secrettoken'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/testrestfull'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/testrestfull'	# your DB connection
+
+app.config['SECRET_KEY'] = 'secrettoken'										# your key to encode the token
 
 db = SQLAlchemy(app)
 
@@ -18,7 +19,7 @@ class User(db.Model):
 	public_id 	= db.Column(db.String(50), unique=True) 	# id for who want to decode the token . like an fake id. is not the exactly sequential id. to make harder to figureout how many user exists and the next or previous number off an user
 	name 		= db.Column(db.String(50))					# just the name for the login
 	password 	= db.Column(db.String(80))					# key for the login. need to be 80 characters because we will use sha256 to hash
-	permission 	= db.Column(db.Boolean)						# if the user had the permission, he can search or create users
+	permission 	= db.Column(db.Boolean)						# if the user had the permission, he can search or create users. you can set permission as string and put multiples values and than when you verify this column, make sure that you are searching the permission that you need
 
 # you need to have already created the database. in this case the database called testrestfull
 # after than use the python commands to generate the table with the especifications above
@@ -28,24 +29,29 @@ class User(db.Model):
 @app.route('/login')
 def login():
 
-	auth = request.authorization
+	auth = request.authorization							# using the basic authorization http protocol
 
 	if not auth or not auth.username or not auth.password:
-		return make_response('Could Not Verify', 401, {'WWW-Authenticate' : 'Basic Realm = "Login Required !!"'})
+		return make_response('Could Not Verify', 400, {'WWW-Authenticate' : 'Basic Realm = "Login Required !!"'}), 401
+		# check the login fields
 
 	user = User.query.filter_by(name = auth.username).first()
 
 	if not user:
-		return make_response('Could Not Verify', 401, {'WWW-Authenticate' : 'Basic Realm = "Login Required !!"'})
+		return make_response('Could Not Verify', 400, {'WWW-Authenticate' : 'Basic Realm = "Cannot Find This User !!"'}), 404
+		# try to find the login user in the DB
 
 	if check_password_hash(user.password, auth.password):
-		token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes = 30)}, app.config['SECRET_KEY'])		# generate the token and set a time for the expiration. the secret_key will be used to encode the token
+		token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes = 30)}, app.config['SECRET_KEY'])		
+		# check the password from the login and DB, after than generate the token and set a time for the expiration. the secret_key will be used to encode the token
 
-		return jsonify({'token' : token.decode('UTF-8')})
+		return jsonify({'token' : token.decode('UTF-8')}), 201
 	
-	return make_response('Could Not Verify', 401, {'WWW-Authenticate' : 'Basic Realm = "Login Required !!"'})
+	return make_response('Could Not Verify', 400, {'WWW-Authenticate' : 'Basic Realm = "Wrong Password !!"'})
+	# if the password dosent match
 
 # 3 cases when the login get pass back: when they dont suply any authorization information; when there is no user in the DB; when is the password is incorrect
+
 
 def token_required(f):
 	@wraps(f)
@@ -75,7 +81,7 @@ def token_required(f):
 def get_all_users(current_user):
 
 	if not current_user.permission:
-		return jsonify({'message' : 'You Need Permission To Do That !!'})
+		return jsonify({'message' : 'You Need Permission To Do That !!'}), 401
 
 	users = User.query.all()							# sqlAlchemy syntax
 
@@ -88,11 +94,11 @@ def get_all_users(current_user):
 		user_data['name']			= user.name
 		user_data['password']		= user.password
 		user_data['permission']		= user.permission
-		# the left side is the response from the BD and the right side is our new dictonary for this values
+		# the left side is the response from the DB and the right side is our new dictonary for this values
 
 		output.append(user_data)						# bind the results with our new object created previusly
 
-	return jsonify({'user' : output})
+	return jsonify({'user' : output}), 200
 
 
 @app.route('/user/<public_id>', methods = ['GET'])
@@ -100,7 +106,7 @@ def get_all_users(current_user):
 def get_one_user(current_user, public_id):
 
 	if not current_user.permission:
-		return jsonify({'message' : 'You Need Permission To Do That !!'})
+		return jsonify({'message' : 'You Need Permission To Do That !!'}), 401
 
 	user = User.query.filter_by(public_id = public_id).first()
 
@@ -112,11 +118,11 @@ def get_one_user(current_user, public_id):
 		user_data['password']		= user.password
 		user_data['permission']		= user.permission
 
-		return jsonify({'user' : user_data})
+		return jsonify({'user' : user_data}), 200
 
 	else:
 
-		return jsonify({'message' : 'User Not Found !!'})
+		return jsonify({'message' : 'User Not Found !!'}), 404
 
 
 @app.route('/user', methods=['POST'])
@@ -124,7 +130,7 @@ def get_one_user(current_user, public_id):
 def create_user(current_user):
 
 	if not current_user.permission:
-		return jsonify({'message' : 'You Need Permission To Do That !!'})
+		return jsonify({'message' : 'You Need Permission To Do That !!'}), 401
 
 	data = request.get_json()								
 	# pass the json object data to the variable called data
@@ -144,7 +150,7 @@ def create_user(current_user):
 
 	# in postman you only need to pass a json with name and password
 
-	return jsonify({'message' : 'New User Added !!'})
+	return jsonify({'message' : 'New User Added !!'}), 201
 
 
 @app.route('/user/<public_id>', methods = ['PUT'])
@@ -152,7 +158,7 @@ def create_user(current_user):
 def update_user(current_user, public_id):
 
 	if not current_user.permission:
-		return jsonify({'message' : 'You Need Permission To Do That !!'})
+		return jsonify({'message' : 'You Need Permission To Do That !!'}), 401
 
 	user = User.query.filter_by(public_id = public_id).first()
 
@@ -161,11 +167,11 @@ def update_user(current_user, public_id):
 		user.permission = True
 		db.session.commit()
 
-		return jsonify({'message' : 'User Successfully Updated'})
+		return jsonify({'message' : 'User Successfully Updated'}), 202
 
 	else:
 
-		return jsonify({'message' : 'User Not Found !!'})
+		return jsonify({'message' : 'User Not Found !!'}), 404
 
 
 @app.route('/user/<public_id>', methods = ['DELETE'])
@@ -173,7 +179,7 @@ def update_user(current_user, public_id):
 def delete_user(current_user, public_id):
 
 	if not current_user.permission:
-		return jsonify({'message' : 'You Need Permission To Do That !!'})
+		return jsonify({'message' : 'You Need Permission To Do That !!'}), 401
 
 	user = User.query.filter_by(public_id = public_id).first()
 
@@ -182,11 +188,11 @@ def delete_user(current_user, public_id):
 		db.session.delete(user)
 		db.session.commit()
 
-		return jsonify({'message' : 'User Successfully Deleted'})
+		return jsonify({'message' : 'User Successfully Deleted'}), 202
 
 	else:
 
-		return jsonify({'message' : 'User Not Found !!'})
+		return jsonify({'message' : 'User Not Found !!'}), 404
 
 
 if __name__ == '__main__':
